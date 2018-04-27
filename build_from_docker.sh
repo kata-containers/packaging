ecf467faa7e87537a760e547863869a8b5f62cba
Jose Carlos Venegas Munoz <jose.carlos.venegas.munoz@intel.com>
2018-04-27 09:57:15 -0500

    ci: Add basic test build packages locally
    Build runtime locally
    
    Signed-off-by: Jose Carlos Venegas Munoz <jose.carlos.venegas.munoz@intel.com>

---
+++ build_from_docker.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+set -x
+set -e
+
+cache_dir=${PWD}/obs-cache
+#where packaing repo lives
+packaging_repo_dir="${PWD}"
+#where results will be stored
+host_datadir="${PWD}/pkgs"
+obs_image="obs-kata"
+
+sudo docker build \
+	--build-arg http_proxy="${http_proxy}" \
+	--build-arg https_proxy="${https_proxy}" \
+	-t $obs_image .
+
+pushd kata-containers-image/ >> /dev/null
+	echo ./build_image.sh
+popd >> /dev/null
+
+sudo docker run \
+	--rm \
+	-v "${HOME}/.ssh":/root/.ssh \
+	-v "${HOME}/.gitconfig":/root/.gitconfig \
+	-v /etc/profile:/etc/profile \
+	--env http_proxy="${http_proxy}" \
+	--env https_proxy="${https_proxy}" \
+	--env no_proxy="${no_proxy}" \
+	-v "${HOME}/.bashrc":/root/.bashrc \
+	-v "$cache_dir":/var/tmp/osbuild-packagecache/ \
+	-v "$packaging_repo_dir":${packaging_repo_dir} \
+	-v "$host_datadir":/var/packaging \
+	-v "$HOME/.oscrc":/root/.oscrc \
+	-it "$obs_image" bash -c "${packaging_repo_dir}/build_all.sh"
