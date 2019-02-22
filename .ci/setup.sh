3e11d6c203b082ddbf1cc59719ec0fbae828621c
Julio Montes <julio.montes@intel.com>
2019-02-22 08:18:28 -0600

    fixme: fixme
    fixme fixme
    
    Signed-off-by: Julio Montes <julio.montes@intel.com>

--- .ci/setup.sh
+++ .ci/setup.sh
@@ -14,6 +14,10 @@ source /etc/os-release
 echo "Setup script for packaging"
 
 if [ "$ID" == ubuntu ]; then
+	echo "Updating the system"
+	sudo apt-get update -y
+	sudo apt-get upgrade -y
+
 	echo "Install snap dependencies"
 	sudo apt-get install -y snapd snapcraft
 
